80c6be55b537e557c3958bfbaef622799eaebe5c
Caspet04 <caspet@edu.umea.se>
2022-04-28 16:16:41 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,124 @@
+<script>
+	import VideoThumbnail from "./VideoThumbnail.svelte";
+	import data from "./data.js";
+	import { randomItem } from "./utility";
+
+	function getRandomJustAired() {
+		return randomItem(data.RELEASING);
+	}
+
+	function getRandomRecommended() {
+		return randomItem(data.FINISHED);
+	}
+
+	function getRandomTrending() {
+		return randomItem(data.RELEASING);
+	}
+
+	function getRandomToCome() {
+		return randomItem(data.NOT_YET_RELEASED);
+	}
+</script>
+
+<main>
+	<div id="navbar" />
+
+	<div id="banner">
+		<div><h1>Aniflix</h1></div>
+	</div>
+
+	<div id="category-list">
+		<div class="category" id="just-aired">
+			<h3 class="category-title">Just Aired</h3>
+			<div class="category-content">
+				{#each Array(10) as i}
+					<VideoThumbnail data={getRandomJustAired()} />
+				{/each}
+			</div>
+		</div>
+		<div class="category" id="recommended">
+			<h3 class="category-title">Recommended</h3>
+			<div class="category-content">
+				{#each Array(10) as i}
+					<VideoThumbnail data={getRandomRecommended()} />
+				{/each}
+			</div>
+		</div>
+		<div class="category" id="trending">
+			<h3 class="category-title">Trending</h3>
+			<div class="category-content">
+				{#each Array(10) as i}
+					<VideoThumbnail data={getRandomTrending()} />
+				{/each}
+			</div>
+		</div>
+
+		<div class="category" id="to-come">
+			<h3 class="category-title">To Come</h3>
+			<div class="category-content">
+				{#each Array(10) as i}
+					<VideoThumbnail data={getRandomToCome()} />
+				{/each}
+			</div>
+		</div>
+	</div>
+</main>
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		background-color: rgb(0, 0, 60);
+	}
+
+	#navbar {
+		position: sticky;
+		top: 0;
+		left: 0;
+		right: 0;
+		height: 50px;
+		background: darkblue;
+	}
+
+	#banner {
+		padding-top: 20px;
+		padding-bottom: 20px;
+		display: flex;
+		width: 100%;
+		justify-content: center;
+		overflow-x: hidden;
+		background-color:rgb(0, 0, 20);
+	}
+	
+	#banner div h1 {
+		color: white;
+		margin-bottom: 2em;
+		font-size: 3em;
+	}
+
+	#banner div {
+		display: flex;
+		align-items: flex-end;
+		justify-content: center;
+		width: 1024px;
+		height: 576px;
+		background-image: url(https://live.staticflickr.com/7450/10612779376_9b93277b9e_b.jpg);
+		box-shadow: 0 0 40px 40px rgb(0, 0, 20) inset;
+	}
+
+	.category {
+		margin-left: 10px;
+		margin-right: 10px;
+	}
+
+	.category-title {
+		color: white;
+		font-size: 1.4em;
+	}
+
+	.category-content {
+		display: flex;
+		overflow-x: scroll;
+		align-items: center;
+	}
+</style>
---
+++ src/VideoThumbnail.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let data;
+    let mouseOver = false;
+</script>
+
+<div class="container">
+    <img src="{data.coverImage.large}" alt="" on:mouseenter="{() => {mouseOver = true;}}" on:mouseleave="{() => {mouseOver = false;}}" />
+    <div class="hover-info" style="display: {mouseOver ? 'block' : 'none'};">
+    </div>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        margin: 5px;
+        border-radius: 10px;
+        overflow: hidden;
+        min-width: min-content;
+        box-sizing: border-box;
+    }
+
+    .hover-info {
+        position: relative;
+        width: 100px;
+        height: 100px;
+        background-color: red;
+    }
+</style>
