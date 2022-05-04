4771683a96ea01e28c4282860aeb0dc84ab42009
Caspet04 <caspet@edu.umea.se>
2022-05-04 10:14:58 +0200

    Created basic test

--- src/components/VideoThumbnail.svelte
+++ src/components/VideoThumbnail.svelte
@@ -1,126 +1,121 @@
 <!-- TODO: Refactor and create comments -->
 <script>
-    import { onMount } from "svelte";
-    import { truncate } from "../js/utility.js";
-
     export let data;
     export let func;
-    let mouseover = false;
-    let container;
-    let right;
-
-    export function resize() {
-        let innerWidth =
-            window.innerWidth ||
-            document.documentElement.clientWidth ||
-            document.body.clientWidth;
-        right = container.getBoundingClientRect().x > innerWidth - 650;
-    }
-
-    onMount(resize);
-    window.addEventListener("resize", resize);
 </script>
 
-<div
-    bind:this={container}
-    class="container {right ? 'right' : 'left'} {mouseover ? 'mouseover' : ''}"
->
-    <img
-        src={data.cover_image}
-        alt=""
-        on:mouseenter={() => {
-            mouseover = true;
-        }}
-        on:mouseleave={() => {
-            mouseover = false;
-        }}
-        on:click={() => {
-            if (func) {
-                func(data);
-            }
-        }}
-    />
+<div class="container">
+    <div class="cover">
+        <img
+            src={data.cover_image}
+            alt=""
+            on:click={() => {
+                if (func) {
+                    func(data);
+                }
+            }}
+        />
+        <div class="play-symbol">
+            <!-- TODO: Add a play symbol -->
+        </div>
+    </div>
+    <h3 class="cover-title">{data.title}</h3>
     <div class="mouseover-info">
-        <h2>{data.title}</h2>
-        <p>{@html truncate(data.description, 200)}</p>
+        <h2 class="info-title">{data.title}</h2>
+        <h5 class="info-episodes">
+            Episodes: {data.episodes_aired}/{data.episodes_total}
+        </h5>
+        <p class="info-description">{@html data.description}</p>
+        <div class="info-stats">
+            <p class="stats-score">{data.score}</p>
+            <p class="stats-status">{data.status}</p>
+            <p class="stats-genre">{data.genres}</p>
+        </div>
     </div>
 </div>
 
 <style>
-    :root {
-        --image-margin: 10px;
-        --image-width: 200px;
-        --image-height: 300px;
-        --border-thickness: 4px;
-        --total-width: calc(
-            var(--image-width) + 2 * var(--image-margin) + 2 *
-                var(--border-thickness)
-        );
-        --info-width: 400px;
-    }
-
     .container {
-        display: flex;
+        position: relative;
+        width: min-content;
+    }
 
-        min-width: var(--total-width);
-        width: var(--total-width);
+    .cover {
+        position: relative;
 
-        box-sizing: border-box;
-        margin: 10px 5px;
-
-        border-radius: 10px;
-        border: solid var(--border-thickness) rgb(0, 0, 60);
+        width: 200px;
+        height: 300px;
+        /* NOTE: This is set due to the fact that they are in a grid and grids set
+                 automatic mins */
+        min-width: 200xp;
+        min-height: 300px;
 
         overflow: hidden;
 
-        background-color: darkblue;
+        border-radius: 20px;
     }
 
-    .right.container {
-        flex-direction: row-reverse;
+    .cover:hover > .play-symbol {
+        visibility: visible;
     }
 
-    .container.mouseover {
-        box-sizing: border-box;
-        z-index: 2;
-        width: calc(var(--info-width) + var(--total-width));
-        border: solid var(--border-thickness) black;
+    .play-symbol {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+
+        background-color: rgba(0, 0, 0, 0.4);
+
+        visibility: hidden;
     }
 
-    .left.container.mouseover {
-        margin-right: calc(0px - var(--info-width) - var(--total-width));
-    }
-
-    .right.container.mouseover {
-        /* HACK: The 5px is needed for some reason*/
-        margin-left: calc(5px - var(--info-width));
-    }
-
-    .container img {
-        width: var(--image-width);
-        min-width: var(--image-width);
-        height: var(--image-height);
-        min-height: var(--image-height);
-
-        padding: var(--image-margin);
-
-        object-fit: cover;
-        overflow: hidden;
-
-        border-radius: 10px;
-    }
-
-    .mouseover-info {
-        width: var(--info-width);
-        height: var(--image-height);
-        overflow: hidden;
-
+    .cover-title {
+        margin-top: 0.1em;
+        margin-bottom: 0.5em;
         color: white;
     }
 
-    .container p {
-        position: relative;
-        font-size: 1.4em;
-        line-height: 1.4em;
+    .container:hover > .mouseover-info {
+        visibility: visible;
+    }
+
+    .mouseover-info {
+        position: absolute;
+        top: 30px;
+        left: 210px;
+
+        width: 300px;
+        height: 400px;
+
+        padding: 10px 0;
+
+        border-radius: 15px;
+
+        color: white;
+        background-color: black;
+
+        visibility: hidden;
+        z-index: 2;
+    }
+
+    .info-title {
+        padding: 0 10px;
+    }
+
+    .info-episodes {
+        background-color: grey;
+        padding: 5px 10px;
+    }
+
+    .info-description {
+        padding: 0px 10px;
+        height: 200px;
+        overflow: hidden;
+    }
+
+    .info-stats {
+        padding: 0 10px;
     }
 </style>
