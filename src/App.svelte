ff66204ff6b683a5eec5100ee633098dc51f7240
Caspet04 <caspet@edu.umea.se>
2022-05-14 11:14:33 +0200

    Feat(player): Added basic iframe youtube player

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,34 @@
 <script>
     import Category from "./components/Category.svelte";
     import Banner from "./components/Banner.svelte";
+    import Youtube from "./components/Youtube.svelte";
     import data from "./data/data";
 
+    let currentData;
     // This is just to make it easier to change all category functions at once if need be
-    function itemClicked(data) {}
+    function itemClicked(data) {
+        currentData = data;
+    }
+
+    function resetItemClicked() {
+        currentData = null;
+    }
 </script>
 
 <main>
     <Banner clickFunction={itemClicked} />
+
+    <div id="overlay-container">
+        {#if currentData != null}
+            <div
+                class="overlay"
+                id="player-overlay"
+                on:click={resetItemClicked}
+            >
+                <Youtube videoId={currentData.trailer.id} />
+            </div>
+        {/if}
+    </div>
 
     <div id="category-list">
         <Category
@@ -48,5 +68,41 @@
         margin: 0;
         padding: 0;
         background-color: rgb(0, 0, 60);
+    }
+
+    #overlay-container {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+
+        /* Guarantee that the overlay is at the top */
+        z-index: 1000;
+
+        pointer-events: none;
+    }
+
+    .overlay {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+
+        pointer-events: all;
+    }
+
+    #player-overlay {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        background: rgba(0, 0, 0, 0.5);
+    }
+
+    #category-list {
+        width: 90vw;
+        margin: auto;
     }
 </style>
---
+++ src/components/Youtube.svelte
@@ -0,0 +1,35 @@
+<!--
+    TODO: Switch to Vime.js instead as a dependency
+    NOTE: It is possible that I don't need to style the iframe
+    and can move on to another project
+-->
+<script>
+    export let videoId;
+
+    // var tag = document.createElement("script");
+    // tag.src = "https://www.youtube.com/iframe_api";
+
+    // var firstScriptTag = document.getElementsByTagName("script")[0];
+    // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // let player;
+
+    // function onYouTubeIframeAPIReady() {
+    //     player = new YT.Player("player", {
+    //         videoId: id,
+    //         events: {
+    //             onReady: onPlayerReady,
+    //             onStateChange: onPlayerStateChange,
+    //         },
+    //     });
+    // }
+</script>
+
+<iframe
+    id="player"
+    title="Something"
+    width="720"
+    height="480"
+    src="https://www.youtube.com/embed/{videoId}?controls=0&enablejsapi=1"
+    frameborder="0"
+/>
