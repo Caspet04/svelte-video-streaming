475602adb724ed4ed22ac982cd7c00d71c5a74fc
Caspet04 <caspet@edu.umea.se>
2022-05-27 16:27:21 +0200

    Feat(player): Allows player fullscreen
    In addition, removed unused code and comments

--- src/components/Youtube.svelte
+++ src/components/Youtube.svelte
@@ -1,28 +1,5 @@
-<!--
-    TODO: Switch to Vime.js instead as a dependency
-    NOTE: It is possible that I don't need to style the iframe
-    and can move on to another project
--->
 <script>
     export let videoId;
-
-    // var tag = document.createElement("script");
-    // tag.src = "https://www.youtube.com/iframe_api";
-
-    // var firstScriptTag = document.getElementsByTagName("script")[0];
-    // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-    // let player;
-
-    // function onYouTubeIframeAPIReady() {
-    //     player = new YT.Player("player", {
-    //         videoId: id,
-    //         events: {
-    //             onReady: onPlayerReady,
-    //             onStateChange: onPlayerStateChange,
-    //         },
-    //     });
-    // }
 </script>
 
 <iframe
@@ -30,6 +7,7 @@
     title="Something"
     width="720"
     height="480"
-    src="https://www.youtube.com/embed/{videoId}?controls=0&enablejsapi=1"
+    src="https://www.youtube.com/embed/{videoId}"
     frameborder="0"
+    allow="fullscreen"
 />
